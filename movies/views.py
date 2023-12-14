9bcea9fc4e2543b2612ebb7749b89e4873d0dad8
Marco Torres <marcoantonio.torres@icloud.com>
2023-12-13 20:19:57 -0500

    added a variable to return data from all the movie objects, this stops us from having to use hard coded data. In our render method we make the third variable a dictionary to avoid an query error.

--- movies/views.py
+++ movies/views.py
@@ -1,30 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-data = {
-    'movies': [
-        {
-            'id': 5,
-            'title': 'Jaws',
-            'year': 1969
-        },
-        {
-            'id': 6,
-            'title': 'Sharknado',
-            'year': 1600
-        },
-        {
-            'id': 7,
-            'title': 'The Meg',
-            'year': 2000
-        },
-    ]
-}
+# we import Movie from .models to access the objects within
+from .models import Movie
 
 # The view then takes a request
 def movies(request):
+
+    # We assing the method to variable data to refer to all the movie objects 
+    data = Movie.objects.all()
+
     # Takes in three arguments 
-    return render(request, 'movies/movies.html', data)
+    return render(request, 'movies/movies.html', {'movies': data})
 
 def home(request):
     return HttpResponse('Home page')
+
+# we pass 'id' as extra data to this view
+def detail(request, id):
+    # We can ask for a certain primary key based on pk and id
+    data = Movie.objects.get(pk=id)
+    return render(request, 'movies/detail.html', {'movie': data})
